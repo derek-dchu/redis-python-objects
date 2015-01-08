e5903b273eef9ae3477cd232e1ef8943bf7c77ae
Derek Hu <derek.dchu@gmail.com>
2015-01-07 23:44:04 -0500

    Create RInt class

--- redis_objects.py
+++ redis_objects.py
@@ -21,3 +21,26 @@ class RStr:
 
     def ttl(self):
         return r.ttl(self.key)
+
+
+class RInt(RStr):
+    """
+    A class represents an integer (which is always string inside redis) of redis.
+    """
+    def __init__(self, key, num):
+        super(RInt, self).__init__(key, num)
+
+    def __str__(self):
+        return r.get(self.key).decode('utf-8')
+
+    def incr(self):
+        r.incr(self.key)
+
+    def incrby(self, num):
+        r.incr(self.key, num)
+
+    def decr(self):
+        r.decr(self.key)
+
+    def decrby(self, num):
+        r.decr(self.key, num)
