c3d07f2598be18a715300079c911843206ba7f5c
Derek Hu <derek.dchu@gmail.com>
2015-01-07 23:44:26 -0500

    Create unit test for RInt class

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 import redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-from redis_objects import rstr
+from redis_objects import RStr, RInt
 
 
 class RStringTest(unittest.TestCase):
@@ -10,17 +10,11 @@ class RStringTest(unittest.TestCase):
     def setUp(self):
         self.key = "msg"
         self.msg = "This is a test message"
-        self.rstr_msg = rstr(self.key, self.msg)
+        self.rstr_msg = RStr(self.key, self.msg)
         
     def test_print(self):
         self.assertEqual(self.rstr_msg.__str__(), "This is a test message")
-        
-    def test_incr(self):
-        self.assertRaises(redis.exceptions.ResponseError, self.rstr_msg.incr)
-        
-    def test_incrby(self):
-        self.assertRaises(redis.exceptions.ResponseError, self.rstr_msg.incrby, 10)
-        
+             
     def test_exists(self):
         self.assertTrue(self.rstr_msg.exists())
 
@@ -30,6 +24,30 @@ class RStringTest(unittest.TestCase):
     def test_del(self):
         del(self.rstr_msg)
         self.assertIsNone(r.get(self.key))
+
+
+class RIntTest(unittest.TestCase):
+
+    def setUp(self):
+        self.key = "num"
+        self.num = 100
+        self.rint = RInt(self.key, self.num)
+
+    def test_incr(self):
+        self.rint.incr()
+        self.assertEqual(self.rint.__str__(), "101")
+
+    def test_incrby(self):
+        self.rint.incrby(100)
+        self.assertEqual(self.rint.__str__(), "200")
+
+    def test_decr(self):
+        self.rint.decr()
+        self.assertEqual(self.rint.__str__(), "99")
+
+    def test_decrby(self):
+        self.rint.decrby(200)
+        self.assertEqual(self.rint.__str__(), "-100")
 
 
 if __name__ == '__main__':
